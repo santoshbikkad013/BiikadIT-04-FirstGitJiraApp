ba8c49ba628fb03c1807fbbcb71cbdbc910c3e12
Bhagyashrichavan12345 <chavanbhagyashrip@gmail.com>
2022-11-11 11:47:22 +0530

    Fir-2:create required package structure

--- FirstGitJiraApp/src/main/java/com/BikkadIT/FirstGitJiraAppApplication.java
+++ FirstGitJiraApp/src/main/java/com/BikkadIT/FirstGitJiraAppApplication.java
@@ -3,11 +3,15 @@ package com.BikkadIT;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.core.recovery.ResilientSyslogOutputStream;
+
 @SpringBootApplication
 public class FirstGitJiraAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstGitJiraAppApplication.class, args);
+		
+		System.out.println("Working file");
 	}
 
 }
